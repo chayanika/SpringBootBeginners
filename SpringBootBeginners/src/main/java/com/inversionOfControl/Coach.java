30166dfb4ece7371e7deb3d0f2f56ec840a803c6
Chayanika Ghosh <chayanika.ghosh@sirionlabs.local>
2019-09-10 22:45:20 +0530

    Spring

---
+++ SpringBootBeginners/src/main/java/com/inversionOfControl/Coach.java
@@ -0,0 +1,5 @@
+package com.inversionOfControl;
+
+public interface Coach {
+    public String getDailyWorkout();
+}
---
+++ SpringBootBeginners/src/main/java/com/inversionOfControl/HelloSpringApp.java
@@ -0,0 +1,21 @@
+package com.inversionOfControl;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+        // File file = new File( "D:/My Projects/UdemySpring/src/main/resources/applicationContext.xml");
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
+}
